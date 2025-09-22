36679e925fa530d24c6c81527764b43445aba8fa
GermanMT <germanoctako@gmail.com>
2025-09-22 19:43:28 +0200

    feat: add read book by id

--- src/main/java/com/example/library/book/controller/BookController.java
+++ src/main/java/com/example/library/book/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.example.library.book.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,10 @@ public class BookController {
     @PostMapping
     public void createBook(@RequestBody BookDTO bookDTO) {
         bookService.saveBook(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO readBook(@PathVariable Long id) {
+        return bookService.getBookById(id);
     }
 }
--- src/main/java/com/example/library/book/service/BookService.java
+++ src/main/java/com/example/library/book/service/BookService.java
@@ -3,6 +3,7 @@ package com.example.library.book.service;
 import org.springframework.stereotype.Service;
 
 import com.example.library.book.dto.BookDTO;
+import com.example.library.book.exception.BookNotFoundException;
 import com.example.library.book.mapper.BookMapper;
 import com.example.library.book.model.Book;
 import com.example.library.book.repository.BookRepository;
@@ -21,5 +22,11 @@ public class BookService {
     public void saveBook(BookDTO bookDTO) {
         Book book = bookMapper.toEntity(bookDTO);
         bookRepository.save(book);
+    }
+
+    public BookDTO getBookById(Long id) {
+        Book book = bookRepository.findById(id)
+        .orElseThrow(() -> new BookNotFoundException(id));
+        return bookMapper.toDTO(book);
     }
 }
