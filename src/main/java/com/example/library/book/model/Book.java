b857b556722e961020fd95586f3b4354a6fc5d98
GermanMT <germanoctako@gmail.com>
2025-09-22 18:18:53 +0200

    feat: add book entity model

---
+++ src/main/java/com/example/library/book/model/Book.java
@@ -0,0 +1,29 @@
+package com.example.library.book.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String author;
+
+    @NotBlank
+    @Column(unique=true)
+    private String ISBN;
+}
