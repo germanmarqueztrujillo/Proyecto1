1e36fa28bd4f6bb3d16dc5eaa7fe8a9f413d6599
GermanMT <germanoctako@gmail.com>
2025-09-22 19:27:42 +0200

    feat: add book controller and create endpoint

---
+++ src/main/java/com/example/library/book/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.example.library.book.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.library.book.dto.BookDTO;
+import com.example.library.book.service.BookService;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public void createBook(@RequestBody BookDTO bookDTO) {
+        bookService.saveBook(bookDTO);
+    }
+}
